6f91d9d969eff09a30b9ec876d678174557acb3d
LukasZmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-08-01 14:01:49 +0200

    pritnln, print, printf

--- Zaklady/src/MojaPrvaTrieda.java
+++ Zaklady/src/MojaPrvaTrieda.java
@@ -3,8 +3,18 @@ public class MojaPrvaTrieda {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Lukas");
+		System.out.println("Elct");
 		System.out.println("AHoj Java");
+		System.out.println(12 + 8);
+		System.out.println("12" + "8");
+		System.out.println("12 + 8 = " + 12 + 8);
+		System.out.println("\"Lukas\"");
+		System.out.println("Programovanie \nJava");
 		
+		System.out.print("Lukas");
+		System.out.print("Elct");
+		
+		System.out.printf("\nDnes je %s. %d. augusta. %.2f", "Pondelok", 1, 12.3);		
 	}
-
 }
