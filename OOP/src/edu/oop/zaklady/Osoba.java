5bd7f9149268ef9056d30fdbb7fd6a80b26347f7
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-08-04 10:53:55 +0200

    trieda osoba
    konstruktory
    metody

--- OOP/src/edu/oop/zaklady/Osoba.java
+++ OOP/src/edu/oop/zaklady/Osoba.java
@@ -1,5 +1,69 @@
 package edu.oop.zaklady;
 
 class Osoba {
+	
+	//instancne premenne
+	//fields
+	String meno;
+	String priezvisko;
+	int vek;
+	//m - muz, z - zena, n - nezname
+	char pohlavie;
 
+	//konstruktory
+	
+	Osoba(){
+		meno = "nezname";
+		priezvisko = "nezname";
+		vek = 10;
+		pohlavie = 'n';
+	}
+	
+	Osoba(String meno, String priezvisko, int vek, char pohlavie){
+		this.meno = meno;
+		this.priezvisko = priezvisko;
+		if(vek > 0) {
+			this.vek = vek;			
+		}else {
+			this.vek = 10;
+		}
+		this.pohlavie = pohlavie;
+	}
+	
+	Osoba(String meno, String priezvisko){
+		this(meno, priezvisko, 10, 'n');
+	}
+	
+//	Osoba(String meno, String priezvisko){
+//		this.meno = meno;
+//		this.priezvisko = priezvisko;
+//		this.vek = 10;
+//		this.pohlavie = 'n';
+//	}
+	//metody
+	
+	void info() {
+		System.out.println(celeMeno());
+		System.out.println("Vek: " + vek);
+		System.out.println("Pohlavie: " + pohlavieSlovom());
+	}
+	
+	String celeMeno() {
+		return meno + " " + priezvisko;
+	}
+	
+	String pohlavieSlovom() {
+		switch(pohlavie) {
+		case 'm':
+			return "muz";
+		case 'z':
+			return "zena";
+		case 'n':
+			return "nezname";
+		default:
+			return "-";
+		}
+	}
+	
+	
 }
--- OOP/src/edu/oop/zaklady/Tester.java
+++ OOP/src/edu/oop/zaklady/Tester.java
@@ -4,6 +4,43 @@ public class Tester {
 
 	public static void main(String[] args) {
 		
+		//deklaracia objektu
+		Osoba osoba;
+		
+		//inicializacia objektu
+		osoba = new Osoba();
+		
+		osoba.info();
+		
+		Osoba osoba2 = new Osoba();
+		
+		osoba.meno = "Jan";
+		osoba.priezvisko = "Hrasko";
+		osoba.pohlavie = 'm';
+		osoba.vek = 24;
+		
+		osoba2.meno = "Ruzena";
+		osoba2.priezvisko = "Sipkova";
+		osoba2.vek = 45;
+		osoba2.pohlavie = 'z';
+		
+		
+		System.out.println(osoba.celeMeno());
+		System.out.println(osoba2.celeMeno());
+		System.out.println(osoba.pohlavieSlovom());
+		
+		osoba2.info();
+		
+		System.out.println("-------------");
+		
+		Osoba os = new Osoba("Fero", "Mravec", 34, 'm');
+		os.info();
+		
+		Osoba os2 = new Osoba("Maja", "Vcela");
+		os2.info();
+		
+		
+		
 
 	}
 
